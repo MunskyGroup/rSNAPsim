9cc9885bf8787e01ad9e3da5e4b8b746452a6661
William Raymond <wsraymon@rams.colostate.edu>
2019-05-02 18:39:57 -0600

    Added U's to aa_table

--- rSNAPsim.py
+++ rSNAPsim.py
@@ -229,7 +229,24 @@ class rSNAPsim():
             'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
             'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
             'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
-            'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',}
+            'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+            
+            'AUA':'I', 'AUC':'I', 'AUU':'I', 'AUG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T',
+            'AAC':'N', 'AAU':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGU':'S', 'AGA':'R', 'AGG':'R',
+            'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P',
+            'CAC':'H', 'CAU':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R',
+            'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A',
+            'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G',
+            'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S',
+            'UUC':'F', 'UUU':'F', 'UUA':'L', 'UUG':'L',
+            'UAC':'Y', 'UAU':'Y', 'UAA':'*', 'UAG':'*',
+            'UGC':'C', 'UGU':'C', 'UGA':'*', 'UGG':'W',}
 
         self.aatable_r = {'A':['GCA', 'GCC', 'GCG', 'GCT'],
                           'R':['CGA', 'CGC', 'CGG', 'CGT','AGG','AGA'],
