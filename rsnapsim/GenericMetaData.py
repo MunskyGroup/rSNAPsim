4335af93b61afa6cb58c2c154e6b3f1d0f41e7c8
William Raymond <wsraymon@rams.colostate.edu>
2021-08-13 15:20:48 -0600

    linted IntensityAnalyses and MetaData
    TODO: IntensityAnalyses.get_frags has to be refactored at some point

--- rsnapsim/GenericMetaData.py
+++ rsnapsim/GenericMetaData.py
@@ -5,23 +5,35 @@ Created on Thu Dec 17 17:48:33 2020
 @author: William Raymond
 """
 
-import rsnapsim as rSNAPsim
 import time
-import os
 import sys
 import platform
-'''
-Method to tag solution objects with some metadata
-'''
+import rsnapsim as rSNAPsim
+
 class GenericMetaData():
+    '''
+    Provides a metatdata dictionary to the user about rsnapsim files or
+    models being generated.
+
+    Attributes
+    ----------
+
+    id
+    created_at
+    rss_version
+    platform
+    python_version
+
+    '''
     def __init__(self):
         self.id = ''
-        self.created_at = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime( time.time() ))
-        self.user = os.path.expanduser("~")
+        self.created_at = time.strftime('%Y-%m-%d %H:%M:%S',
+                                        time.localtime(time.time()))
+        #self.user = os.path.expanduser("~")
         self.rss_version = rSNAPsim.__version__
         self.platform = platform.platform()
         self.python_version = sys.version
-        
+
     def get(self):
         '''
         generate and return a metadata dictionary for a solver object
@@ -29,8 +41,20 @@ class GenericMetaData():
         Returns
         -------
         dict
-            a dictionary of metadata such as solution id, time ran, user, platform and rss version.
+            a dictionary of metadata such as solution id, time ran, user,
+            platform and rss version.
 
         '''
         return self.__dict__
-        
+
+    def get_as_str(self):
+        metadata = self.get()
+        # convert to a metadata string
+        meta_str = '######################################################'
+        meta_str += 'model ID : ' + metadata['id'] + '\n'
+        meta_str += 'files created at : ' + metadata['created_at']+ '\n'
+        meta_str += 'platform : ' + metadata['platform']+ '\n'
+        meta_str += 'python version : ' + metadata['python_version']+ '\n'
+        meta_str += 'rsnapsim version : ' + metadata['rss_version']+ '\n'
+        meta_str += '#####################################################'
+        return meta_str
